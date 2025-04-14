5f3e117bde284fac1e298497e51a7b64ea9e7b6e
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-04-14 12:10:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on localhost: 6000`);
+    
+});
